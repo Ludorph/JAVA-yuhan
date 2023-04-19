25969f74f2b40a6d8707149c444fd488a0679720
Ludorph <ludorph@naver.com>
2023-04-19 10:44:41 +0900

    Test1

---
+++ src/test/method/methodTest2.java
@@ -0,0 +1,17 @@
+package test.method;
+
+public class methodTest2 {
+	
+	static void add(int n1, int n2)
+	{
+		int sum = n1 + n2;
+		System.out.printf("%d + %d = %d\n", n1, n2, sum);
+	}
+
+	public static void main(String[] args) {
+		add(1000,2000);
+		
+
+	}
+
+}
